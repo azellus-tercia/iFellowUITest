3353ddfe62c9dd8e2d7eb026711504efd3b29004
Azellus Tercia <azellustercia@gmail.com>
2022-10-15 16:43:46 +0500

    Initial commit.

--- src/main/java/utils/Configuration.java
+++ src/main/java/utils/Configuration.java
@@ -3,7 +3,6 @@ package utils;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
@@ -17,15 +16,15 @@ public abstract class Configuration {
     static {
         properties = new Properties();
         try (InputStream inputStream = Configuration.class.getResourceAsStream(CONFIGURATION_FILE)) {
-//            properties.load(inputStream);
-            properties.load(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            if (inputStream != null) {
+                properties.load(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
+            }
         } catch (IOException e) {
             throw new RuntimeException("Failed to read file" + CONFIGURATION_FILE, e);
         }
     }
 
-    // Эта строка смотрит на входящие данные из вне. Допустим можно указать данные в Jenkins, если таких данных нет то брать из проперти файла
     public static String getConfigurationValue(String key) {
-        return ((System.getProperty(key) == null) ? properties.getProperty(key) : System.getProperty(key));
+        return properties.getProperty(key);
     }
 }
